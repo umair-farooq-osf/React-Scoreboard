2bb2aec440831fdf059de653d9f1c004464a0c34
Umair Farooq <umair.farooq@osf-commerce.com>
2019-05-16 13:41:22 +0500

    React Components: Managing State and Data Flow section code

--- src/components/App.js
+++ src/components/App.js
@@ -1,27 +1,53 @@
 import React, { Component } from 'react';
 import Header from './Header';
 import Player from './Player';
+import AddPlayerForm from './AddPlayerForm';
 
 class App extends Component {
     state = {
         players: [
             {
                 name: "Guil",
+                score: 0,
                 id: 1
             },
             {
                 name: "Treasure",
+                score: 0,
                 id: 2
             },
             {
                 name: "Ashley",
+                score: 0,
                 id: 3
             },
             {
                 name: "James",
+                score: 0,
                 id: 4
             }
         ]
+    }
+
+    // player id counter
+    prevPlayerId = 4;
+
+    handleScoreChange = (index, delta) => {
+        this.setState(prevState => ({
+            score: prevState.players[index].score += delta
+        }));
+    }
+
+    handleAddPlayer = (name) => {
+        this.setState(prevState => {
+            return {
+                players: prevState.players.concat({
+                    name,
+                    score: 0,
+                    id: this.prevPlayerId += 1
+                })
+            }
+        });
     }
 
     handleRemovePlayer = (id) => {
@@ -37,18 +63,25 @@ class App extends Component {
             <div className="scoreboard">
                 <Header
                     title="Scoreboard"
-                    totalPlayers={this.state.players.length}
+                    players={this.state.players}
                 />
 
                 { /* PLayers List */}
-                {this.state.players.map(player =>
+                {this.state.players.map((player, index) =>
                     <Player
                         name={player.name}
+                        score={player.score}
                         id={player.id}
                         key={player.id.toString()}
+                        index={index}
+                        changeScore={this.handleScoreChange}
                         removePlayer={this.handleRemovePlayer}
                     />
                 )}
+
+                <AddPlayerForm
+                    addPlayer={this.handleAddPlayer}
+                />
             </div>
         );
     }
