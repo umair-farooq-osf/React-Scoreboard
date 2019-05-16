dbbbf16e69c03c6fbefea48d24e81097d05c3c0e
Umair Farooq <umair.farooq@osf-commerce.com>
2019-05-16 10:16:22 +0500

    React Basics: Thinking in Components section code

--- app.js
+++ app.js
@@ -1,15 +1,44 @@
-const desc = 'I just learned how to create a React Node and render it into the DOM';
-const titleId = 'main-title';
-const name = 'Guil';
+const Header = () => {
+    return (
+        <header>
+            <h1>Scoreboard</h1>
+            <span className="stats">Players: 1</span>
+        </header>
+    );
+};
 
-const header = (
-    <header>
-        <h1 id={titleId} title="This is a title.">{name}'s First React Element</h1>
-        <p className="main-desc">{desc}</p>
-    </header>
-);
+const Player = () => {
+    return (
+        <div className="player">
+            <span className="player-name">
+                Guil
+            </span>
+            <Counter />
+        </div>
+    );
+};
 
+const Counter = () => {
+    return (
+        <div className="counter">
+            <button className="counter-action decrement"> - </button>
+            <span className="counter-score">35</span>
+            <button className="counter-action increment"> + </button>
+        </div>
+    );
+};
+
+const App = () => {
+    return (
+        <div className="scoreboard">
+            <Header />
+
+            { /* PLayers List */}
+            <Player />
+        </div>
+    );
+}
 ReactDOM.render(
-    header,
+    <App />,
     document.getElementById('root')
 );
